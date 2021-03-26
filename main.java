911ffd4b9d84f21c70c35ba8a02c0d03274a7927
PADMAG6 <padmavathi.g6@gmail.com>
2021-03-26 13:20:41 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+package com.internsharla.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       System.out.print("hello internshala"); //this is my first code in java
+    }
+}
