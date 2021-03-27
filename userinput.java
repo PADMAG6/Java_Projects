181a1c2bc7a8765479d4d4e2557ab90d58924258
PADMAG6 <padmavathi.g6@gmail.com>
2021-03-27 09:33:05 +0530

    Update userinput.java

--- userinput.java
+++ userinput.java
@@ -13,3 +13,34 @@ public class Userinput {
 	}
 
 }
+
+
+
+
+package com.internsharla.java;
+
+import java.util.Scanner;
+
+public class Userinput {
+	public static void main(String[] args) {
+		
+		String name;
+		int age;
+
+		// Write your code here
+		Scanner sca = new Scanner(System.in);
+		//name = sca.nextLine();
+		System.out.println("my name is:");
+		name = sca.nextLine();
+
+		//age = sca.nextInt();
+		System.out.println("my age is: ");
+		age = sca.nextInt();
+
+		System.out.println("my name is "+ name);
+		System.out.println("mys age is " + age);
+
+		sca.close();
+
+	}
+}
