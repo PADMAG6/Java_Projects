24cc3ec2659be22e743c8f0f078581c41c47f6c1
PADMAG6 <padmavathi.g6@gmail.com>
2021-03-27 22:46:29 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,23 @@
+// for loop
+public class Main {
+    public static void main(String[] args) {
+        for (int ctr = 1 ; ctr > 7; ctr++) {
+            System.out.println("Hello Java");
+        }
+    }
+}
+
+//while loop
+package com.internsharla.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        while (num > 0) {
+            System.out.println("hello "+ num);
+            num--;
+        }
+    }
+}
