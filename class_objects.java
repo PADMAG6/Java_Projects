b92dd06b681a1dbf0fa55d1871ab27c07c52b491
PADMAG6 <padmavathi.g6@gmail.com>
2021-03-31 09:03:12 +0530

    Update class_objects.java

--- class_objects.java
+++ class_objects.java
@@ -3,24 +3,21 @@
 package com.internsharla.java;
 
 public class Rectangle {
-	public int length;
-	public int breadth;
+	public int length;           //here length is instance variable
+	public int breadth;         // breadth instance varible
 
 
     public int getArea() {
-	   int area = length * breadth;
+	   int area = length * breadth;            //here area is local variable, it is decleard in method
 	   return area;
 	//System.out.println(length * breadth);
     }
     public int getPrimeter() {
-	   int perimeter = 2 * (length + breadth);
+	   int perimeter = 2 * (length + breadth);  //perimeter is local variable
 	    return perimeter;
 	//System.out.println(length + breadth);
     }
 }
-
-
-
 
 
 //main.java
@@ -30,13 +27,13 @@ public class Main {
 
 public static void main(String[] args) {
       Rectangle proper = new Rectangle();
-      proper.length=34;
+      proper.length=34;                       
       proper.breadth = 56;
 		System.out.println(proper.getArea());
       proper.getPrimeter();
 		//System.out.println(proper.length);
 
-      Rectangle r1 = new Rectangle();
+      Rectangle r1 = new Rectangle();          //here Rectanlg is class, r1 is reference variable, new Rectagle() is object
       r1.length = 67;
       r1.breadth = 23;
 		System.out.println(r1.getArea());
