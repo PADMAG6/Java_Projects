ed3e37bd358895dfe7c8d394c7b60a1ee668edf4
PADMAG6 <padmavathi.g6@gmail.com>
2021-03-31 08:56:33 +0530

    Update class_objects.java

--- class_objects.java
+++ class_objects.java
@@ -7,34 +7,41 @@ public class Rectangle {
 	public int breadth;
 
 
-public void area() {
-	System.out.println(length * breadth);
+    public int getArea() {
+	   int area = length * breadth;
+	   return area;
+	//System.out.println(length * breadth);
+    }
+    public int getPrimeter() {
+	   int perimeter = 2 * (length + breadth);
+	    return perimeter;
+	//System.out.println(length + breadth);
+    }
 }
-public void primeter() {
-	System.out.println(length + breadth);
-}
-}
+
+
+
 
 
 //main.java
 package com.internsharla.java;
 
-import sun.misc.Cache;
-
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class Main {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
       Rectangle proper = new Rectangle();
       proper.length=34;
       proper.breadth = 56;
-      proper.area();
-      proper.primeter();
-		System.out.println(proper.length);
+		System.out.println(proper.getArea());
+      proper.getPrimeter();
+		//System.out.println(proper.length);
 
-
+      Rectangle r1 = new Rectangle();
+      r1.length = 67;
+      r1.breadth = 23;
+		System.out.println(r1.getArea());
+		System.out.println(r1.getPrimeter());
 	}
 }
+
 
